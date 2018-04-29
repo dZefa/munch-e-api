e8f6e139c3c0179bfbc5eed1dbc46f2a8f15d64e
Daniel Chong <danchong625@gmail.com>
2018-04-29 01:51:13 -0700

    [srvr] initializes user router with register route

---
+++ src/routes/user.router.ts
@@ -0,0 +1,21 @@
+import { Router, Request, Response } from 'express';
+import { matchedData } from 'express-validator/filter';
+import { validationResult } from 'express-validator/check';
+
+import { userRules } from '../rules/user.rules';
+import { UserService } from '../services/user.service';
+import { UserAddModel } from '../db/models/user';
+
+export const userRouter: Router = Router();
+const userService = new UserService();
+
+userRouter.post('/register', userRules['forRegister'], (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) return res.status(422).send({ result: { errors: errors.array() } });
+
+  const payload = matchedData(req) as UserAddModel;
+  const user = userService.register(payload);
+
+  return user.then(user => res.status(201).send({ result: { user } }));
+});
