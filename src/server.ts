ce23bb0f0c2d4a81d496da8e0afa7214531ae9de
Daniel Chong <danchong625@gmail.com>
2018-04-28 19:18:35 -0700

    [srvr] initializes express server

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+
+import logger from './util/logger';
+
+const app: express.Application = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("short"));
+
+app.listen(port, () => {
+  logger(`Listening at http://localhost:${port}/`);
+});
