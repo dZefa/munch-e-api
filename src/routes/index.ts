93c537f6775d89cac888af61cbcaa5edb4385966
Daniel Chong <danchong625@gmail.com>
2018-04-29 01:50:55 -0700

    [srvr] adds user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
-const router: Router = Router();
+import { userRouter } from './user.router';
 
-export default router;
+export const router: Router = Router();
+
+router.use('/user', userRouter);
