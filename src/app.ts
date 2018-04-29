fff546abee1c38f811dab8870affd7579bd6ee65
Daniel Chong <danchong625@gmail.com>
2018-04-28 21:42:41 -0700

    [srvr] implements api router

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,15 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 
+import routes from './routes';
+
 const app: express.Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('short'));
+
+app.use('/api', routes);
 
 export default app;
