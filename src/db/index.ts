fe0d13d00b7418d38a28ea33425802425ae12312
Daniel Chong <danchong625@gmail.com>
2018-04-29 01:50:40 -0700

    [db] refactors out authentication to syncDB method

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,3 @@
 import { default as Sequelize } from 'sequelize';
-import logger from '../util/logger';
 
 export const sequelize = new Sequelize(process.env.DB_URL!);
-
-sequelize.authenticate().then(() => {
-  logger(`DB authenticated!`);
-});
