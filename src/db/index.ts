3c995dddbe9aafa259fdce179dd91f2841d51621
Daniel Chong <danchong625@gmail.com>
2018-04-29 22:43:10 -0700

    [db] removes logging when in testing environment

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,5 @@
 import { default as Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(process.env.DB_URL!);
+const logging = process.env.NODE_ENV === 'test' ? false : true;
+
+export const sequelize = new Sequelize(process.env.DB_URL!, { logging });
