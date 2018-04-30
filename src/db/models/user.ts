2f7082e81de671950e07b4b1da44826e41c9013c
Daniel Chong <danchong625@gmail.com>
2018-04-29 20:46:39 -0700

    [db] adds association to Bio model

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -1,13 +1,17 @@
 import { default as Sequelize } from 'sequelize';
 import { sequelize } from '../index';
 
+import { Bio } from './userBio';
+
 export interface UserAddModel {
   email: string;
   password: string;
+  BioId: number | null;
 }
 
 export interface UserModel extends Sequelize.Model<UserModel, UserAddModel> {
   id: number;
+  BioId: number | null;
   email: string;
   password: string;
   createdAt: string;
@@ -17,9 +21,12 @@ export interface UserModel extends Sequelize.Model<UserModel, UserAddModel> {
 export interface UserViewModel {
   id: number;
   email: string;
+  BioId: number | null;
 }
 
 export const User = sequelize.define<UserModel, UserAddModel>('user', {
   email: Sequelize.STRING,
   password: Sequelize.STRING,
 });
+
+User.belongsTo(Bio, { foreignKey: { allowNull: true, name: 'BioId' }, onDelete: 'CASCADE' });
