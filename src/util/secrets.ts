608d12b0d1094717b4c3ee0f544fc2dfd3f45e5c
Daniel Chong <danchong625@gmail.com>
2018-04-29 22:44:38 -0700

    [ops] implements test env file in testing environment

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -1,8 +1,10 @@
-import { default as dotenv } from 'dotenv';
+import dotenv from 'dotenv';
 import path from 'path';
 
 if (process.env.NODE_ENV === 'production') {
   dotenv.config({ path: path.resolve(__dirname, '../../config/.env') });
+} else if (process.env.NODE_ENV === 'test') {
+  dotenv.config({ path: path.resolve(__dirname, '../../config/test.env') });
 } else {
   dotenv.config({ path: path.resolve(__dirname, '../../config/dev.env') });
 }
