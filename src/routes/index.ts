548c55255d0533805e285a4be679f87595387d3a
Daniel Chong <danchong625@gmail.com>
2018-05-18 16:45:08 -0700

    [srvr] adds bio router and implements token middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 
 import { userRouter } from './user.router';
+import { bioRouter } from './bio.router';
+
+import { tokenGuard } from '../middlewares/token-guard';
 
 export const router: Router = Router();
 
 router.use('/user', userRouter);
+
+router.use('/bio', tokenGuard(), bioRouter);
