b6b561980faf6133503866cabcc8188364ca179d
Daniel Chong <danchong625@gmail.com>
2018-05-19 17:52:00 -0700

    [srvr] adds null check to userid and first+lastname check

--- src/rules/bio.rules.ts
+++ src/rules/bio.rules.ts
@@ -1,14 +1,20 @@
 import { check } from 'express-validator/check';
 import { Bio } from '../db/models/userBio';
-import { isDate } from 'util';
 
 export const bioRules = {
   toSetBio: [
     check('UserId')
+      .custom(UserId => UserId && !isNaN(UserId))
+        .withMessage('Invalid UserId or is Null')
       .custom(UserId => Bio.find({ where: { UserId } }).then(b => !!!b))
         .withMessage('Bio has already been set.'),
     check('dob')
-      .custom(dob => isDate(dob))
+      .custom((dob) => {
+        const date = new Date(dob);
+        return date instanceof Date;
+      })
         .withMessage('Invalid Date format'),
+    check('firstName'),
+    check('lastName'),
   ],
 };
