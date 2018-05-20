c49cc53b358728320ce8f17aef24135048fc6e02
Daniel Chong <danchong625@gmail.com>
2018-05-19 19:24:12 -0700

    [db] fixes getBio typing and correctly returns object or viewmodel

--- src/services/bio.service.ts
+++ src/services/bio.service.ts
@@ -15,24 +15,27 @@ export class BioService {
 
   setBio({ firstName, lastName, dob, UserId }: BioAddModel) {
     return Bio.create({ firstName, lastName, dob, UserId })
-      .then((b: BioModel) => this.getBioByUserId(b.id));
+      .then((b: BioModel) => this.getBioById(b.id));
   }
 
   updateBio(updateObj: BioAddModel) {
     return Bio.update(updateObj)
-      .then((result: [number, BioModel[]]) => this.getBioByUserId(result[1][0].id));
+      .then((result: [number, BioModel[]]) => this.getBioById(result[1][0].id));
   }
 
   getBio({ UserId }: BioAddModel) {
     return Bio.findOne({ where: { UserId } })
-      .then(async (b) => {
-        const bio = b && await this.getBioByUserId(b.id);
+      .then((b): BioViewModel | Object => {
+        const bio = b ? this.getBioById(b.id) : false;
+        if (!bio) {
+          return { error: 'Bio not found' };
+        }
 
         return bio;
       });
   }
 
-  getBioByUserId(id: number) {
+  getBioById(id: number) {
     return Bio.findById(id, {
       attributes: BioService.bioAttributes,
     }) as Bluebird<BioViewModel>;
