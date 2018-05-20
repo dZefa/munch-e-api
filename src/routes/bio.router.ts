ed015cbadb59569d35a56a05422fe55d2b1a5c76
Daniel Chong <danchong625@gmail.com>
2018-05-19 19:20:31 -0700

    [srvr] add bio get route

--- src/routes/bio.router.ts
+++ src/routes/bio.router.ts
@@ -4,7 +4,7 @@ import { validationResult } from 'express-validator/check';
 
 import { BioService } from '../services/bio.service';
 import { bioRules } from '../rules/bio.rules';
-import { BioAddModel } from '../db/models/userBio';
+import { BioAddModel, BioModel } from '../db/models/userBio';
 
 export const bioRouter: Router = Router();
 const bioService = new BioService();
@@ -20,4 +20,19 @@ bioRouter.post('/set', bioRules['toSetBio'], (req: Request, res: Response) => {
   const bio = bioService.setBio(payload);
 
   return bio.then(bio => res.status(201).send({ result: { bio } }));
+});
+
+bioRouter.get('/get', bioRules['toGetBio'], (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).send({ result: { errors: errors.array() } });
+  }
+
+  const payload = matchedData(req);
+
+  console.log(`this is payload in get bio: ${JSON.stringify(payload)}`);
+  const bio = bioService.getBio({ UserId: payload.id } as BioAddModel);
+
+  return bio.then(bio => res.status(200).send({ result: { bio } }));
 });
